31f521a4f4252120e3a14e137b3e71ea49b1af05
isdn7 <isdn718@gmail.com>
2025-07-21 14:34:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ if version:
         
         streamlit_marquee(
             content=marquee_content,
-            velocity=0,  # 속도 조절 (숫자가 작을수록 느려짐)
+            velocity=-10,  # 속도 조절 (숫자가 작을수록 느려짐)
             loop=0,
             background="#222222",
             font_size="18px",
